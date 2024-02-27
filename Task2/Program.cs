1f381eb4e4c50dc8bb5acd3a5aa00be442535bbc
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-27 18:36:37 +1000

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿void Main()
+{
+	Console.Write("Введите число m: ");
+	int m = int.Parse(Console.ReadLine());
+
+	Console.Write("Введите число n: ");
+	int n = int.Parse(Console.ReadLine());
+
+	Console.WriteLine(AckermannFunction(m, n));
+}
+
+static int AckermannFunction(int m, int n)
+{
+	if (m == 0)
+	{
+		return n + 1;
+	}
+	else if (n == 0)
+	{
+		return AckermannFunction(m - 1, 1);
+	}
+	else
+	{
+		return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+	}
+}
+
+Main();
