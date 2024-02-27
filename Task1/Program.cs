86b57b60c858b8cec12b6dd442cd0d796a0950b9
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-27 18:32:22 +1000

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿void Main(){
+  System.Console.WriteLine("Введите числа M и N");
+  int m = Convert.ToInt32(Console.ReadLine());
+  int n = Convert.ToInt32(Console.ReadLine());
+  PrintDigits(m,n);
+}
+
+void PrintDigits(int start,int end){
+  if (start<=end){
+    System.Console.Write(start+" ");
+    PrintDigits(start+1,end);
+  }
+}
+
+Main();
