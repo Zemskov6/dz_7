2b6d1563707e6932eb3f889341ca9c9c54ee594e
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-27 18:44:53 +1000

    Задача3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿void Main()
+{
+	int[] arr = {1,2,3,4,5,6,7,8};
+	Printarray(arr,0);
+
+}
+
+void Printarray(int[] array, int index)
+{
+	if (index < array.Length)
+	{
+		Printarray(array, index + 1);
+		Console.Write(array[index] + " ");
+
+	}
+}
+
+Main();
+
